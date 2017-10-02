c22eb48bc48cdb0ef96c8ed93702ecd5f75eb4a0
nardtree <gim.kobayashi@gmail.com>
2017-10-02 17:14:05 +0900

    update

--- gpu_kmeans.py
+++ gpu_kmeans.py
@@ -5,6 +5,11 @@ import cupy as cp
 import numpy as np
 import pickle
 
+# here is defines 
+vector_size = 100
+cluster_num = 200
+max_iter = 120
+
 index_meta = pickle.loads( open('make_word_vector/index_meta.pkl', 'rb').read() )
 
 arrays = []
@@ -21,14 +26,13 @@ for index, meta in sorted(index_meta.items(), key=lambda x:x[0]):
 x_all = cp.array(arrays) 
 x_allnorm = cp.linalg.norm(x_all, axis=(1,) )
 
-clusters =  [cp.random.randn(100) for n in range(100)] 
+clusters = [cp.random.randn(vector_size) for n in range(cluster_num)] 
 print( 'initial', clusters )
 
 rams = None
-for it in range(400):
+for it in range(max_iter):
   cossimsall = []
   for e, cluster in enumerate(clusters):
-    #print( e )
     cluster_norm = cp.linalg.norm(cluster) 
     cluster_all = cluster * x_all
     norm = cluster_norm * x_allnorm
@@ -39,15 +43,13 @@ for it in range(400):
 
   cossimsall = [ c.tolist() for c in cossimsall ]
   cc = np.array( cossimsall )
-  #print( cc.shape )
-  #print( cc.T )
   ams = np.argmax(cc.T, axis=1)
   if rams is not None and np.array_equal(rams,ams):
     print( ams )
     break
 
   if rams is not None:
-    print('differ', it, (ams - rams).tolist()) 
+    print('differ', it, (ams - rams).tolist()[:200]) 
   print('now iter', it,  ams )
   print('old ams', it, rams )
 
@@ -67,4 +69,5 @@ for it in range(400):
   clusters = means
 
 ams = [ (en, ms) for en, ms in enumerate(ams.tolist()) ]
+import json
 open('ams.json', 'w').write( json.dumps(ams) )
